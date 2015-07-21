ec47d580db7868076ff32f4cfad7696cb039f01a
stskc <noracat09@gmail.com>
2015-07-21 22:23:26 +0900

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,37 +4,44 @@ describe "Static pages" do
 
   subject { page }
 
-#  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
   describe "Home page" do
     before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading)     { 'Help' }
+    let(:page_title)  { '' }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Help') }
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading)     { 'About' }
+    let(:page_title)  { '' }
 
-    it { should have_content('About') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| About') }
   end
 
   describe "Contact" do
     before { visit contact_path}
+    let(:heading)     { 'Contact' }
+    let(:page_title)  { '' }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('')) }
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Contact') }
   end
 end
